966a92af1962bd3aa53957942bcf83cacaddf8b8
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-05-08 22:57:16 +0200

    Employees Overview

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent {
-
+  employees = [
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      img: '../../assets/images/Employee1.png',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      img: '../../assets/images/Employee2.png',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      img: '../../assets/images/Employee3.png',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      img: '../../assets/images/Employee4.png',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+  ];
 }
