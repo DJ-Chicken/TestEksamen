4d804adedd4c079cbe96ce6f64a541570c2df694
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-05-08 23:15:36 +0200

    Projects imported

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {
-
+  projects = [
+    {
+      title: 'Project 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      img: '',
+    },
+  ];
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'Project 1',
+      img: '../../assets/images/Project.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+      title: 'Project 2',
+      img: '../../assets/images/Project.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+      title: 'Project 3',
+      img: '../../assets/images/Project.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+  ];
 }
