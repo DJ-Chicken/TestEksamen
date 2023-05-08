0538e071a6b3b9d23a24adac9eae1fd12d6fce19
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-05-08 22:07:48 +0200

    Simple Setup +Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    EmployeesComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#004264",
+        notWhite: "#fbf6ec",
+      },
+    },
+  },
+  plugins: [],
+};
